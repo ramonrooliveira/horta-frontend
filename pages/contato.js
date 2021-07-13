0beea13353cb54bef839132a63ea8d3fc000a908
Ramon Raupp <ramonrooliveira@gmail.com>
2021-07-13 09:06:44 -0300

    feat: add about and contact pages

--- pages/contato.js
+++ pages/contato.js
@@ -2,25 +2,42 @@ import React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { fetchAPI } from "../lib/api";
+import Image from "../components/image";
 
-const Contato = ({ homepage, header, footer }) => {
+const Contato = ({ homepage, header, footer, contact }) => {
   return (
     <Layout header={header} footer={footer}>
       <Seo seo={homepage.seo} />
+      <Image
+          image={contact?.image}
+          style={{
+            position: "static",
+            height: 200,
+          }}
+        />
+        <div>
+          <p>{contact?.contactBlock.title}</p>
+          <p>{contact?.contactBlock.addressFirstLine}</p>
+          <p>{contact?.contactBlock.addressSecondLine}</p>
+          <p>tel: {contact?.contactBlock.phones}</p>
+          <p>{contact?.contactBlock.email}</p>
+          <p>instragram: {contact?.contactBlock.instagram}</p>
+        </div>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
   // Run API calls in parallel
-  const [homepage, header, footer] = await Promise.all([
+  const [homepage, header, footer, contact] = await Promise.all([
     fetchAPI("/homepage"),
     fetchAPI("/header"),
-    fetchAPI("/footer")
+    fetchAPI("/footer"),
+    fetchAPI("/contact")
   ]);
 
   return {
-    props: { homepage, header, footer },
+    props: { homepage, header, footer, contact },
     revalidate: 1,
   };
 }
--- pages/sobre.js
+++ pages/sobre.js
@@ -2,25 +2,49 @@ import React from "react";
 import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { fetchAPI } from "../lib/api";
+import Image from "../components/image"
+import Link from "next/link";
 
-const Sobre = ({ homepage, header, footer }) => {
+const Sobre = ({ homepage, header, footer, about }) => {
   return (
     <Layout header={header} footer={footer}>
       <Seo seo={homepage.seo} />
+        <div className="about__container">
+          <div className="left-image-section">
+            <Image
+              image={about?.image}
+              style={{
+                position: "static",
+                height: 380,
+              }}
+            />
+          </div>
+          <div className="about__text-section">
+            <p>{about?.title}</p>
+            <p>{about?.text}</p>
+            <Link href={about?.button.url}>
+              <button 
+                type="button"
+                className="primary-button"
+              >{about?.button.label}</button>
+            </Link>
+          </div>
+        </div>
     </Layout>
   );
 };
 
 export async function getStaticProps() {
   // Run API calls in parallel
-  const [homepage, header, footer] = await Promise.all([
+  const [homepage, header, footer, about] = await Promise.all([
     fetchAPI("/homepage"),
     fetchAPI("/header"),
-    fetchAPI("/footer")
+    fetchAPI("/footer"),
+    fetchAPI("/about")
   ]);
 
   return {
-    props: { homepage, header, footer },
+    props: { homepage, header, footer, about },
     revalidate: 1,
   };
 }
